cc6f338aa2794803f876c88b1bf28a5f0fa85379
Petr Andreev <drakula2805@gmail.com>
2024-07-16 11:21:32 +0300

    Добавлен env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
+import os
 import allure
 import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selene import browser
 from selenium.webdriver.chrome.options import Options
@@ -8,37 +10,47 @@ from utils import attach
 
 
 DEFAULT_BROWSER_VERSION = "121.0"
-def pytest_addoptions(parser):
+
+
+def pytest_addoption(parser):
     parser.addoption(
         '--browser_version',
-        default='121.0'
+        default='100.0'
     )
 
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
 @pytest.fixture(scope='function', autouse=True)
-def browser_management(request):
-    with allure.step(f'Открываем браузер на главной странице: "https://demoqa.com"'):
-        browser_version = request.config.getoption('--browser_version')
-        browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-        options = Options()
-        selenoid_capabilities = {
-            "browserName": 'chrome',
-            "browserVersion": browser_version,
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": True
-            }
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
         }
+    }
+    options.capabilities.update(selenoid_capabilities)
 
-        options.capabilities.update(selenoid_capabilities)
-        driver = webdriver.Remote(
-            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-            options=options)
-        browser.config.driver = driver
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
-    browser.config.base_url = 'https://demoqa.com'
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    options.page_load_strategy = 'eager'
+    browser.config.base_url = 'https://demoqa.com/'
+    browser.config.driver = driver
     # options.add_argument("--headless")
 
     yield
